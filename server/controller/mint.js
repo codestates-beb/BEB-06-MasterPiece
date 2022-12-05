ba917f3d75f9d7ca3df977e07f0bc7460b6c5aec
mynameisdaun <daun9870.jung@gmail.com>
2022-12-05 09:49:47 +0900

    bug fix

--- server/controller/mint.js
+++ server/controller/mint.js
@@ -2,13 +2,12 @@ const db = require("../db");
 
 module.exports = {
 	find: async (req, res) => {
-		const response = {};
 		const query = `
             select b.id                 as pieceMintingId,
                    b.contract_address   as contractAddress,
                    b.collection_name    as collectionName,
                    b.holder_address     as holderAddress,
-                   b.name               as name,
+                   b.nft_name           as name,
                    b.description        as description,
                    b.img_url            as imgUrl,
                    b.piece_total_count  as pieceTotalCount,
@@ -20,23 +19,19 @@ module.exports = {
                          n.contract_address,
                          n.collection_name,
                          n.holder_address,
-                         m.name,
-                         m.description,
-                         m.img_url,
+                         n.nft_name,
+                         n.description,
+                         n.img_url,
                          p.piece_total_count,
                          date_format(p.sale_end_date_time, '%y-%m-%dT00:00:00.000Z') as sale_end_date_time,
                          p.status,
                          p.price
                   from nft_piece_minting p
                            left join nft n on p.nft_id = n.id
-                           left join nft_meta_data m on p.nft_id = m.nft_id
-                           left join nft_tag t on p.nft_id = t.nft_id
                   where p.sale_end_date_time >= current_timestamp()
                     and p.status = 0
                     and p.delete_yn = 0
-                    and n.delete_yn = 0
-                    and m.delete_yn = 0
-                    and t.delete_yn = 0) b
+                    and n.delete_yn = 0) b
                      left join nft_piece a on b.id = a.nft_id`;
 		db.query(query, (err, rows) => {
 			if (err) throw err;
@@ -51,8 +46,7 @@ module.exports = {
 		let query = `
             select p.nft_id
             from nft_piece_minting p
-            where p.id = ${pieceMintingId}
-		`;
+            where p.id = ${pieceMintingId}`;
 		await db.query(query, async (err, result) => {
 			if (err) throw err;
 			query = `
