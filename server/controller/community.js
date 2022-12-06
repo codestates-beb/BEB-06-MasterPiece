5e99d1e772c9b65ed151289212d8089101178ed9
mynameisdaun <daun9870.jung@gmail.com>
2022-12-06 10:58:09 +0900

    vote result

--- server/controller/community.js
+++ server/controller/community.js
@@ -1,13 +1,13 @@
 const db = require("../db");
 
 module.exports = {
-  community: async (req, res) => {
-    const query = `
+	community: async (req, res) => {
+		const query = `
             select a.id               as postId,
                    n.collection_name  as collectionName,
                    n.nft_name         as nftName,
                    a.title            as title,
-				   a.address		  as address,
+                   a.address          as address,
                    a.agenda_type      as type,
                    a.create_date_time as createDateTime
             from dao_agenda a
@@ -15,53 +15,53 @@ module.exports = {
             where a.delete_yn = 0
               and n.delete_yn = 0
 		`;
-    db.query(query, (err, rows) => {
-      if (err) throw err;
-      return res.send(rows);
-    });
-  },
+		db.query(query, (err, rows) => {
+			if (err) throw err;
+			return res.send(rows);
+		});
+	},
 
-  agenda: async (req, res) => {
-    const { postId } = req.params;
-    const {
-      collectionName,
-      nftName,
-      address,
-      title,
-      description,
-      type,
-      duration,
-    } = req.body;
-    let query = `
+	agenda: async (req, res) => {
+		const {postId} = req.params;
+		const {
+			collectionName,
+			nftName,
+			address,
+			title,
+			description,
+			type,
+			duration,
+		} = req.body;
+		let query = `
             select n.id
             from nft n
             where n.collection_name = ?
               and n.nft_name = ?
               and n.delete_yn = 0
 		`;
-    db.query(query, [collectionName, nftName], (err, rows) => {
-      if (err) throw err;
-      if (rows.length === 0) throw err;
-      const nftId = rows[0].id;
-      query = `
+		db.query(query, [collectionName, nftName], (err, rows) => {
+			if (err) throw err;
+			if (rows.length === 0) throw err;
+			const nftId = rows[0].id;
+			query = `
                 insert into dao_agenda(id, nft_id, address, title, content, agenda_type, staking_duration)
                 values (?, ?, ?, ?, ?, ?, ?)
 			`;
-      db.query(
-        query,
-        [postId, nftId, address, title, description, type, duration],
-        (err, rows) => {
-          if (err) throw err;
-          console.log(rows);
-          res.send("ok");
-        }
-      );
-    });
-  },
+			db.query(
+				query,
+				[postId, nftId, address, title, description, type, duration],
+				(err, rows) => {
+					if (err) throw err;
+					console.log(rows);
+					res.send("ok");
+				}
+			);
+		});
+	},
 
-  description: async (req, res) => {
-    const { postId } = req.params;
-    const query = `
+	description: async (req, res) => {
+		const {postId} = req.params;
+		const query = `
             select a.id                                                                       as postId,
                    n.collection_name                                                          as collectionName,
                    n.nft_name                                                                 as nftName,
@@ -79,22 +79,56 @@ module.exports = {
               and a.delete_yn = 0
               and n.delete_yn = 0
 		`;
-    db.query(query, [postId, postId, postId], (err, rows) => {
-      if (err) throw err;
-      res.send(rows);
-    });
-  },
+		db.query(query, [postId, postId, postId], (err, rows) => {
+			if (err) throw err;
+			res.send(rows);
+		});
+	},
 
-  vote: async (req, res) => {
-    const { postId } = req.params;
-    const { vote, address } = req.body;
-    const query = `
+	vote: async (req, res) => {
+		const {postId} = req.params;
+		const {vote, address} = req.body;
+		const query = `
             insert into dao_agenda_voting (agenda_id, address, agree)
             values (?, ?, ?)
 		`;
-    db.query(query, [postId, address, vote], (err, rows) => {
-      if (err) throw err;
-      return res.send("ok");
-    });
-  },
-};
+		db.query(query, [postId, address, vote], (err, rows) => {
+			if (err) throw err;
+			return res.send("ok");
+		});
+	},
+
+	isVoted: async (req, res) => {
+		const {postId, address} = req.params;
+		const query = `
+            select count(*) as voted
+            from dao_agenda_voting
+            where agenda_id = ?
+              and address = ?
+		`;
+		db.query(query, [postId, address], (err, rows) => {
+			if (err) throw err;
+			if (rows[0].voted > 0) {
+				return res.send("ok");
+			}
+			return res.send("no");
+		});
+	},
+
+	result: async (req, res) => {
+		const {postId} = req.params;
+		const query = `
+            select count(*)                              as total,
+                   count(case when agree = 1 then 1 end) as agree,
+                   count(case when agree = 0 then 1 end) as disagree
+            from dao_agenda_voting
+            where agenda_id = ?;
+		`;
+		db.query(query, [postId], (err, rows) => {
+			if (err) throw err;
+			res.send(rows);
+		});
+
+	}
+}
+;
--- server/router/community.js
+++ server/router/community.js
@@ -4,13 +4,15 @@ const {
 	agenda,
 	community,
 	description,
-	vote,
+	vote, isVoted, result,
 } = require("../controller/community")
 
 router.get("", community);
 router.post("/:postId", agenda)
 router.get("/:postId", description);
 router.post("/:postId/vote", vote);
+router.get("/:postId/vote/result", result);
+router.get("/:postId/vote/:address", isVoted);
 
 module.exports = router;
 
