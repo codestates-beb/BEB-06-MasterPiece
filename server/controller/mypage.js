cec99d6f5fa05e18813b849d1cbff730c52cfcd3
mynameisdaun <daun9870.jung@gmail.com>
2022-12-06 16:01:27 +0900

    collection and nftName

--- server/controller/mypage.js
+++ server/controller/mypage.js
@@ -18,10 +18,14 @@ module.exports = {
 	profile: async (req, res) => {
 		const {address} = req.params;
 		const query = `
-            select address,
-                   profile_url
-            from profile
-            where address = ?
+            select p.address,
+                   p.profile_url as profileUrl,
+                   n.collection_name as collectionName,
+                   n.nft_name as nftName
+            from profile p
+                     left join nft_piece np on np.address = p.address
+                     left join nft n on np.nft_id = n.id
+            where p.address = ?
 		`
 		db.query(query, [address], (err, rows) => {
 			if (err) throw err;
