87e4d18c26d5828cb5ab516420176546cec74635
mynameisdaun <daun9870.jung@gmail.com>
2022-12-06 23:05:21 +0900

    profile url

--- server/controller/mypage.js
+++ server/controller/mypage.js
@@ -3,13 +3,13 @@ const db = require("../db");
 module.exports = {
 	setting: async (req, res) => {
 		const {address} = req.params;
-		const {profile} = req.body;
+		const {profileUrl} = req.body;
 		const query = `
             update profile
             set profile_url = ?
             where address = ?
 		`;
-		db.query(query, [profile, address], (err, rows) => {
+		db.query(query, [profileUrl, address], (err, rows) => {
 			if (err) throw err;
 			console.log(rows);
 			res.send("ok");
